1b719b6df8537dc19058366bee14e6b3a136e353
bestv5666 <lixr873@163.com>
2026-02-13 22:32:17 +0800

    fix: fix packaging issues for both Python and Java versions
    Python:
    - Remove --windowed flag to show error messages
    - Add auto-open browser functionality
    
    Java:
    - Add BrowserLauncher component to auto-open browser
    - Add StaticController to serve static files
    - Configure resource packaging in pom.xml

--- app.py
+++ app.py
@@ -191,4 +191,13 @@ ensure_data_dir()
 schedule_index_updates()
 
 if __name__ == '__main__':
+    import webbrowser
+    import threading
+    import time
+    
+    def open_browser():
+        time.sleep(1.5)
+        webbrowser.open('http://localhost:5000')
+    
+    threading.Thread(target=open_browser, daemon=True).start()
     app.run(host='0.0.0.0', port=5000, debug=True)
